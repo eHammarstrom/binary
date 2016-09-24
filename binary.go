5b4443426f2dbdd2de6bd291ba1c72a78cceba0a
Emil Hammarstrom <emil.a.hammarstrom@gmail.com>
2016-09-25 00:05:50 +0200

    initial commit

---
+++ binary.go
@@ -0,0 +1,60 @@
+package binary
+
+import "math"
+
+func PaddedBinaryArray(source []int, length int) []int {
+	arr := make([]int, length, length)
+
+	for i := 0; i < len(source); i++ {
+		arr[len(arr)-1-i] = source[i]
+	}
+
+	return arr
+}
+
+func IntToBinaryArray(x int) []int {
+	arrLen := int(math.Ceil(math.Log2(float64(x + 1))))
+	arr := make([]int, arrLen, arrLen)
+
+	if x%2 == 0 {
+		arr[int(math.Log2(float64(x)))] = 1
+		return reverseArray(arr)
+	}
+
+	for i := 0; i < arrLen; i++ {
+		if x >= int(math.Pow(2, float64(arrLen-1-i))) {
+			arr[i] = 1
+			x = x - int(math.Pow(2, float64(arrLen-1-i)))
+		}
+	}
+
+	return arr
+}
+
+func BinaryArrayToInt(arr *[]int) int {
+	sum := 0
+
+	iterArr := reverseArray(*arr)
+
+	for i := 0; i < len(iterArr); i++ {
+		if iterArr[i] == 1 {
+			sum += int(math.Pow(2, float64(i)))
+		}
+	}
+
+	return sum
+}
+
+func reverseArray(arr []int) []int {
+	if len(arr) == 0 {
+		return []int{}
+	}
+
+	revArr := make([]int, len(arr), len(arr))
+
+	for i := 0; i < len(arr); i++ {
+		revArr[i] = arr[len(arr)-1-i]
+	}
+
+	return revArr
+}
